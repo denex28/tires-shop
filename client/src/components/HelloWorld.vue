cb7152e1561c6848bc489d2ffb1953a3db773e45
denex28 <ms.tiesto@mail.ru>
2024-02-29 01:48:57 +0300

    button

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
   <div>залупа</div>
   <h1>пупа</h1>
-  <button class="button hit">купить
+  <button type="button" name="НОВИНКА" class="NEW">НОВИНКА
   </button>
+  <button type="button" name="хит" class="hit">ХИТ</button>
+  <button type="button" name="РАСПРОДАЖА" class="sale but">РАСПРОДАЖА</button>
+  <button type="button" name="-20" class="ss">-20%</button>
 </template>
 
 <script lang="ts">
@@ -20,8 +23,29 @@ export default defineComponent({
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
+} 
+button {
+  font-size: 11px;
+  color:white;
+  text-align:center;
+  font-weight: 500;
+  padding: 10px 10px;
+  margin:10px 10px;
+  font-family:sans-serif;
+  border-style: none;
 }
-.class 
+.NEW { 
+background-color:black ;
+}
+.hit {background-color:#3F71BC ; 
+
+}
+.sale {background-color:#9747FF ;
+}
+.ss {background-color:
+#FF6915;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
